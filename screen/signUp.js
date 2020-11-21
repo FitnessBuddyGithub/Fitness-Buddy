1bfbffc11e90bc1bf92247d1cf97c00037746502
Jenna Jiang <tqj5047@gmail.comm>
2020-11-21 10:34:18 -0500

    signup

--- screen/signUp.js
+++ screen/signUp.js
@@ -1,40 +1,75 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, input } from 'react-native';
 import React from 'react';
 import { useForm } from 'react-hook-form'
 import { registerNewUser } from '../store/user'
 import { connect } from 'react-redux'
 import isEmail from "validator/lib/isEmail";
-
+import Constants from 'expo-constants';
 
 function SignUpDC(props) {
   const { register, handleSubmit, watch, errors } = useForm();
   const onSubmit = data => {
+    console.log(data)
     let user = {
       email: data.email,
       password: data.password,
-      gender: data.gender
+      gender: data.Gender
     }
     console.log(user)
     props.newUser(user)
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input name="email"
-        placeholder="Email"
-        // style={{ ...styles.input, borderColor: errors.email && "red" }}
-        ref={register({ required: true, validate: (input) => isEmail(input) })} />
-      <input name="password"
-        placeholder="Password"
-        // style={{ ...styles.input, borderColor: errors.password && "red" }}
-        ref={register({ required: true, minLength: 6, maxLength: 10 })} />
-      <select name="gender" ref={register}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-      </select>
-      <input type="submit" />
-    </form>
+    <View style={styles.container}>
+      <form onSubmit={handleSubmit(onSubmit)} >
+        <input name='email'
+          placeholder="Email"
+          style={{ ...styles.input, borderColor: errors.email ? "red" : "black" }}
+          ref={register({ required: true, validate: (input) => isEmail(input) })} />
+        <input name="password"
+          placeholder="Password"
+          style={{ ...styles.input, borderColor: errors.password ? "red" : "black" }}
+          ref={register({ required: true, minLength: 6, maxLength: 10 })} />
+        <label>
+          Gender:
+        <select name="Gender" ref={register}>
+            <option value="female">female</option>
+            <option value="male">male</option>
+          </select>
+        </label>
+        <input type="submit" />
+      </form>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  label: {
+    color: 'white',
+    margin: 20,
+    marginLeft: 0
+  },
+  button: {
+    marginTop: 40,
+    color: 'white',
+    backgroundColor: 'blue',
+    height: 40,
+    borderRadius: 4,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
+    backgroundColor: 'white'
+  },
+  input: {
+    backgroundColor: 'white',
+    borderColor: 'white',
+    height: 40,
+    padding: 10,
+    borderRadius: 4,
+  }
+});
 
 const mapDispatch = dispatch => ({
   newUser: (user) => dispatch(registerNewUser(user))
